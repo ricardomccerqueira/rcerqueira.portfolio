d59929646867f900edc3668d6fc3a0a114984cb1
ricardo cerqueira <ricardomccerqueira@gmail.com>
2012-12-17 00:07:44 +0000

    experiments

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,6 @@
 Portfolio.app=$.sammy(function(){
 	this.loaderElement	= null;
+	this.appisloaded = false;
 	
 	this.use('Template');
 
@@ -58,13 +59,12 @@ Portfolio.app=$.sammy(function(){
 
 	
 	this.get('#/:filter', function(context){
+		Portfolio.app.appisloaded = true;
 		var filter = this.params['filter'];
 
 		var getSectionContent = _.find(Portfolio.websiteData.sections, function(context){
 			return context.title == filter;
 		});
-
-		console.log(getSectionContent);
 
 		context.render('templates/content.template', {item:getSectionContent}).then(function(content){
 			context.app.swap(content,$('#content'),Portfolio.app.apploaded);
@@ -72,5 +72,51 @@ Portfolio.app=$.sammy(function(){
 
 		$('#nav .selected-menu').removeClass('selected-menu');
 		$('#nav .'+filter).addClass('selected-menu');
+
+		$(".contactButton").attr("href","#/"+filter+"/contactme");
+
+		$('#contactFormHolder').fadeOut("fast");
+	});
+
+	this.get('#/:filter/contactme', function(context){
+		if(Portfolio.app.appisloaded == false){
+			Portfolio.app.appisloaded = true;
+
+			var filter = this.params['filter'];
+
+			var getSectionContent = _.find(Portfolio.websiteData.sections, function(context){
+				return context.title == filter;
+			});
+
+			context.render('templates/content.template', {item:getSectionContent}).then(function(content){
+				context.app.swap(content,$('#content'),Portfolio.app.apploaded);
+			});
+
+			$('#nav .selected-menu').removeClass('selected-menu');
+			$('#nav .'+filter).addClass('selected-menu');
+			
+			$(".contactButton").attr("href","#/"+filter+"/contactme");
+		}
+
+		var scrollYPos = $('#contactFormHolder').offset().top;
+		TweenLite.to(window, 2, {scrollTo:{y:scrollYPos, x:0}, ease:Power4.easeOut})
+
+		setTimeout(function(){
+			$('#contactFormHolder').fadeIn("slow",function(){
+				var scrollYPos = $('#contactFormHolder').offset().top;
+				TweenLite.to(window, 2, {scrollTo:{y:scrollYPos, x:0}, ease:Power4.easeOut})			
+			});
+		},1000);
+
+		var controller 		= $.superscrollorama();
+		var scrollDuration  = 600; 
+
+		controller.addTween('#contactFormHolder', TweenMax.from( $('#contactFormHolder'), 2, {css:{opacity: 0}}), scrollDuration);
+//      controller.addTween('#fade-it', TweenMax.from( $('#fade-it'), .5, {css:{opacity: 0}}), scrollDuration);
+  //    controller.addTween('#fly-it', TweenMax.from( $('#fly-it'), .25, {css:{right:'1000px'}, ease:Quad.easeInOut}), scrollDuration);
+    //  controller.addTween('#spin-it', TweenMax.from( $('#spin-it'), .25, {css:{opacity:0, rotation: 720}, ease:Quad.easeOut}), scrollDuration);
+    //  controller.addTween('#scale-it', TweenMax.fromTo( $('#scale-it'), .25, {css:{opacity:0, fontSize:'20px'}, immediateRender:true, ease:Quad.easeInOut}, {css:{opacity:1, fontSize:'240px'}, ease:Quad.easeInOut}), scrollDuration);
+    //  controller.addTween('#smush-it', TweenMax.fromTo( $('#smush-it'), .25, {css:{opacity:0, 'letter-spacing':'30px'}, immediateRender:true, ease:Quad.easeInOut}, {css:{opacity:1, 'letter-spacing':'-10px'}, ease:Quad.easeInOut}), scrollDuration); // 100 px offset for better timing
+
 	});
 });
